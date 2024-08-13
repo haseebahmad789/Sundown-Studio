68039cd91e36d0872781a7e7c330a5572b8b1b6a
Waqas Ahmad <haseebahmadofficial39@gmail.com>
2024-08-13 18:37:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let hoverContainer = document.querySelector(".hover-container");
+let fixed = document.querySelector(".fixed");
+let elems = document.querySelectorAll(".elem");
+
+hoverContainer.addEventListener("mouseenter", function(){
+    fixed.style.display = "block";
+})
+
+hoverContainer.addEventListener("mouseleave", function(){
+    fixed.style.display = "none";
+})
+
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        var image = e.getAttribute("data-image");
+        fixed.style.backgroundImage = `url(${image})`;
+    })
+})
+
+var loader = document.querySelector("#loader");
+
+setTimeout(function(){
+    loader.style.top = "-100%"
+},4200)
+
+
+
+
+
+
+
+
+
+
+let menu = document.querySelector("nav i");
+let half = document.querySelector("nav .half");
+
+let flag = 0;
+menu.addEventListener("click", function(){
+    if(flag == 0){
+        half.style.left = "0%"
+        flag = 1;
+    }
+    else{
+        half.style.left = "-110%"
+        flag = 0;
+    }
+})
