98753c7f9446fb69de01a9f9f4f7929fb193fb62
Waqas Ahmad <haseebahmadofficial39@gmail.com>
2024-08-13 18:46:42 +0500

    script changes

--- script.js
+++ script.js
@@ -1,75 +1,59 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+function locomotiveAnimation(){
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let hoverContainer = document.querySelector(".hover-container");
-let fixed = document.querySelector(".fixed");
-let elems = document.querySelectorAll(".elem");
-
-hoverContainer.addEventListener("mouseenter", function(){
-    fixed.style.display = "block";
-})
-
-hoverContainer.addEventListener("mouseleave", function(){
-    fixed.style.display = "none";
-})
-
-elems.forEach(function(e){
-    e.addEventListener("mouseenter", function(){
-        var image = e.getAttribute("data-image");
-        fixed.style.backgroundImage = `url(${image})`;
+function hoverAnimation(){
+    let hoverContainer = document.querySelector(".hover-container");
+    let fixed = document.querySelector(".fixed");
+    let elems = document.querySelectorAll(".elem");
+    
+    hoverContainer.addEventListener("mouseenter", function(){
+        fixed.style.display = "block";
     })
-})
+    
+    hoverContainer.addEventListener("mouseleave", function(){
+        fixed.style.display = "none";
+    })
+    
+    elems.forEach(function(e){
+        e.addEventListener("mouseenter", function(){
+            var image = e.getAttribute("data-image");
+            fixed.style.backgroundImage = `url(${image})`;
+        })
+    })
+}
 
-var loader = document.querySelector("#loader");
+function loaderAnimation(){
+    var loader = document.querySelector("#loader");
+    
+    setTimeout(function(){
+        loader.style.top = "-100%"
+    },4200)
+}
 
-setTimeout(function(){
-    loader.style.top = "-100%"
-},4200)
+function hamburgerAnimation(){
+    let menu = document.querySelector("nav i");
+    let half = document.querySelector("nav .half");
+    
+    let flag = 0;
+    menu.addEventListener("click", function(){
+        if(flag == 0){
+            half.style.left = "0%"
+            flag = 1;
+        }
+        else{
+            half.style.left = "-110%"
+            flag = 0;
+        }
+    })
+}
 
 
-
-
-
-
-
-
-
-
-let menu = document.querySelector("nav i");
-let half = document.querySelector("nav .half");
-
-let flag = 0;
-menu.addEventListener("click", function(){
-    if(flag == 0){
-        half.style.left = "0%"
-        flag = 1;
-    }
-    else{
-        half.style.left = "-110%"
-        flag = 0;
-    }
-})
+loaderAnimation();
+hoverAnimation();
+loaderAnimation();
+hamburgerAnimation();
